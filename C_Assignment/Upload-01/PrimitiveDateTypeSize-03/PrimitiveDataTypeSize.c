220941591ac3ba362fa9b3e6a63f8ea95d2916fd
Siddhant-Kshirsagar <iamsiddhant91@gmail.com>
2023-05-14 13:34:40 +0530

    C_Assignment

--- C_Assignment/Upload-01/PrimitiveDateTypeSize-03/PrimitiveDataTypeSize.c
+++ C_Assignment/Upload-01/PrimitiveDateTypeSize-03/PrimitiveDataTypeSize.c
@@ -5,14 +5,14 @@ int main(void)
 	//code 
 	printf("\n\n");
 
-	printf("Size of 'int'			= %lld bytes\n",sizeof(int));
-	printf("Size of 'unsigned int'  = %lld bytes\n",sizeof(unsigned int));
-	printf("Size of 'long'			= %lld bytes\n",sizeof(long));
-	printf("Size of 'long long'		= %lld bytes\n",sizeof(long long));
+	printf("Size of 'int'			= %zd bytes\n",sizeof(int));
+	printf("Size of 'unsigned int'  = %zd bytes\n",sizeof(unsigned int));
+	printf("Size of 'long'			= %zd bytes\n",sizeof(long));
+	printf("Size of 'long long'		= %zd bytes\n",sizeof(long long));
 
-	printf("Size of 'float'			= %lld bytes\n",sizeof(float));
-	printf("Size of 'double'		= %lld bytes\n",sizeof(double));
-	printf("Size of 'long double'	= %lld bytes\n",sizeof(long double));
+	printf("Size of 'float'			= %zd bytes\n",sizeof(float));
+	printf("Size of 'double'		= %zd bytes\n",sizeof(double));
+	printf("Size of 'long double'	= %zd bytes\n",sizeof(long double));
 
 	printf("\n\n");
 	
