d4d816ff8662bf34bb7fe20c7d7abf283ca339d3
Siddhant-Kshirsagar <iamsiddhant91@gmail.com>
2023-10-31 13:05:27 +0530

    Texture_Assignment_Checkerboard

---
+++ 01-OpenGL/01-FFP/02-OGL/14-Texture/03-2DTexture/03-Checkerboard/OGL.c
@@ -0,0 +1,532 @@
+//windows header file
+#include<windows.h> // for win32 API
+#include<stdio.h> // For File IO
+#include<stdlib.h> // For Exit
+
+//OpenGL Header
+#include<gl/GL.h>
+#include<GL/glu.h>
+
+#include"OGL.h"
+
+#define CHECKIMAGEWIDTH 64
+#define CHECKIMAGEHEIGHT 64
+
+//OpenGl Related Global variable
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+
+//Macros 
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+// Link With OpenGL Library
+#pragma comment(lib,"OpenGL32.lib")
+#pragma comment(lib,"glu32.lib")
+
+//Global function declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//Global variable declaration
+DWORD dwStyle = 0;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) }; // initialization of struct => this work on all type (if we want to initialize all value to 0)
+BOOL gbFullscreen = FALSE;
+FILE *gpFILE = NULL;
+
+HWND ghwnd = NULL; // g = global handle of window
+BOOL gbActive = FALSE;
+
+GLuint texture_checkerboard = 0;
+
+// for glTexImage2d parameter
+
+
+GLubyte checkImage[CHECKIMAGEHEIGHT][CHECKIMAGEWIDTH][4];
+
+//Entry Point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//Function declaration
+	int initialize(void);
+	void uninitialize(void);
+	void display(void);
+	void update(void);
+
+	//local variable declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("SGKWindow");
+
+	int iResult = 0;
+
+	BOOL bDone = FALSE; // For Game Loop
+
+	int SystemWindowWidth, SystemWindowHeight;
+	int x_Coordinate, y_Coordinate;
+
+	// get system window width and height
+	SystemWindowWidth = GetSystemMetrics(SM_CXSCREEN);
+	SystemWindowHeight = GetSystemMetrics(SM_CYSCREEN);
+	// get x and y coordinate for show window in center on screen
+	x_Coordinate = (SystemWindowWidth / 2) - (WIN_WIDTH / 2);
+	y_Coordinate = (SystemWindowHeight / 2) - (WIN_HEIGHT / 2);
+
+	//code
+	gpFILE = fopen("Log.txt", "w");
+	if (gpFILE == NULL)
+	{
+		MessageBox(NULL, TEXT("Log file cannot be open"), TEXT("Error"), MB_OK | MB_ICONERROR);
+		exit(0);
+	}
+	fprintf(gpFILE, "Program Started Successfully\n");
+
+	//WNDCLASSEX Initialization
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbWndExtra = 0;
+	wndclass.cbClsExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	//Register WNDCLASSEX
+	RegisterClassEx(&wndclass);
+
+	//Create Window
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("Siddhant Ganesh Kshirsagar"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		x_Coordinate,
+		y_Coordinate,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	//Initialization 
+	iResult = initialize();
+	if (iResult != 0)
+	{
+		MessageBox(hwnd, TEXT("initialize() failed."), TEXT("Error"), MB_OK | MB_ICONERROR);
+		DestroyWindow(hwnd);
+	}
+
+	//show window
+	ShowWindow(hwnd, iCmdShow);
+
+	// set window first in z-order (foreground)
+	SetForegroundWindow(hwnd);
+
+	// set focus on this window(internally send 'WM_SETFOCUS' message to WndProc())
+	SetFocus(hwnd);
+
+	//Game Loop
+	while (bDone == FALSE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				bDone = TRUE;
+			}
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			// code
+			if (gbActive == TRUE) // If Window is in focus then 'WM_SETFOCUS' is send to WndProc() and We Handled this message and set gbActive = true 
+			{
+				//Render
+				display();
+
+				//Update
+				update();
+			}
+
+		}
+	}
+
+	// Uninitialization
+	uninitialize();
+
+	return((int)msg.wParam);
+}
+
+//Callback function defination
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//function declarations
+	void ToggleFullScreen(void);
+	void resize(int, int);
+
+	switch (iMsg)
+	{
+	case WM_SETFOCUS: // when window is in focus (window is activate)
+		gbActive = TRUE;
+		break;
+	case WM_KILLFOCUS: // when window is not in focus(window is deactivate)
+		gbActive = FALSE;
+		break;
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_KEYDOWN:
+	{
+		switch (LOWORD(wParam))
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+		default:
+			break;
+		}
+	}
+	break;
+	case WM_CHAR:
+	{
+		switch (LOWORD(wParam))
+		{
+		case 'F':
+		case 'f':
+			if (gbFullscreen == FALSE)
+			{
+				ToggleFullScreen();
+				gbFullscreen = TRUE;
+			}
+			else
+			{
+				ToggleFullScreen();
+				gbFullscreen = FALSE;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullScreen(void)
+{
+	//local variable declaration
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+
+	//code
+	if (gbFullscreen == FALSE)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+		ShowCursor(FALSE);
+	}
+	else
+	{
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+	}
+}
+
+int initialize(void)
+{
+	//function declarations
+	void resize(int width, int height);
+	void loadGLTexture(void);
+
+	//variable declarations
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex = 0;
+
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	//step 1 :- Initialization of PIXELFORMATDESCRIPTOR
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 32;
+
+	// step 2 :- Get DC 
+	ghdc = GetDC(ghwnd);
+	if (ghdc == NULL)
+	{
+		fprintf(gpFILE, "GetDC() Failed.\n");
+		return(-1);
+	}
+
+	// step 3 :- Os will return pixel format index which is closest match to our pixel format descriptor
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0) // ChoosePixelFormat() return non zero positive on success , on failure return 0
+	{
+		fprintf(gpFILE, "ChoosePixelFormat() Failed.\n");
+		return(-2);
+	}
+
+	// step 4 :- Set obtained pixel format
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+	{
+		fprintf(gpFILE, "SetPixelFormat() Failed.\n");
+		return(-3);
+	}
+
+	// step 5 :- Create OpenGL Context From Device Context(ghdc)
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL)
+	{
+		fprintf(gpFILE, "wglCreateContext() Failed.\n");
+		return(-4);
+	}
+
+	// step 6 :- Make rendering context current
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)
+	{
+		fprintf(gpFILE, "wglMakeCurrent() Failed.\n");
+		return(-5);
+	}
+
+	//for enable depth
+	glShadeModel(GL_SMOOTH); // optional (beautyfication color,light,texture shade)
+	glClearDepth(1.0f); // compulsory
+	glEnable(GL_DEPTH_TEST);// compulsory
+	glDepthFunc(GL_LEQUAL);// compulsory
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // optional (beautyfication for artifact e.g if we draw circle or sphere we see ellipse curve but we don't need that so we disable that)
+
+	// step 7 : - set clear color of window to blue (here OpenGL Start)
+	glClearColor(0.75f, 0.75f, 0.75f, 1.0f);
+
+	loadGLTexture();
+
+	// enable texture
+	glEnable(GL_TEXTURE_2D);
+
+	resize(WIN_WIDTH, WIN_HEIGHT);
+
+	return(0);
+}
+
+void loadGLTexture(void)
+{
+	//local function declaration
+	void makeCheckImage(void);
+	
+	//code
+
+	makeCheckImage();
+
+	// create openGL texture
+	glGenTextures(1, &texture_checkerboard);
+
+	// bind to the generated texture
+	glBindTexture(GL_TEXTURE_2D, texture_checkerboard);
+
+	// decide image pixel alignment and unpacking 
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// set texture parameter
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	// create multiple mipmap images
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, CHECKIMAGEWIDTH, CHECKIMAGEHEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
+
+	// unBind texture
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+}
+
+void makeCheckImage(void)
+{
+	//variable declaration
+	int i, j, c;
+
+	//code
+	for (i = 0; i < CHECKIMAGEHEIGHT; i++)
+	{
+		for (j = 0; j < CHECKIMAGEWIDTH; j++)
+		{
+			c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0))* 255;
+
+			checkImage[i][j][0] = (GLubyte)c;
+			checkImage[i][j][1] = (GLubyte)c;
+			checkImage[i][j][2] = (GLubyte)c;
+			checkImage[i][j][3] = (GLubyte)c;
+
+		}
+	}
+}
+
+void resize(int width, int height)
+{
+	//code
+	if (height <= 0)
+	{
+		height = 1;
+	}
+
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}
+
+void display(void)
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+
+	// cube
+	glLoadIdentity();
+
+	glTranslatef(0.0f, 0.0f, -3.6f);
+
+	glBindTexture(GL_TEXTURE_2D, texture_checkerboard);
+
+
+
+	glBegin(GL_QUADS);
+
+	// front side
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-2.0f, 1.0f, 0.0f);
+
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-2.0f, -1.0f, 0.0f);
+
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(0.0f, -1.0f, 0.0f);
+
+
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(2.41421f, 1.0f, -1.41421f);
+
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(1.0, 1.0f, 0.0f);
+
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, 0.0f);
+
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(2.41421f, -1.0f, -1.41421f);
+	
+
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	SwapBuffers(ghdc);
+}
+
+void update(void)
+{
+	//code
+}
+
+void uninitialize(void)
+{
+	//function declarations
+	void ToggleFullScreen(void);
+	//code
+
+	// If Application is exitting in fullscreen mode
+	if (gbFullscreen == TRUE)
+	{
+		ToggleFullScreen();
+		gbFullscreen = FALSE;
+	}
+
+	//Make ghdc as current context
+	if (wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	//Delete Rendering Context
+	if (ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	// Release the ghdc
+	if (ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+	// Destroy Window
+	if (ghwnd)
+	{
+		DestroyWindow(ghwnd);
+		ghwnd = NULL;
+	}
+	// delete texture
+	if (texture_checkerboard)
+	{
+		glDeleteTextures(1, &texture_checkerboard);
+		texture_checkerboard = 0;
+	}
+
+	// Close log file
+	if (gpFILE)
+	{
+		fprintf(gpFILE, "Program Ended Successfully\n");
+		fclose(gpFILE);
+		gpFILE = NULL;
+	}
+}
+
+
